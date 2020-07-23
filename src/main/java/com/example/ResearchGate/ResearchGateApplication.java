e7af6f5a363d307439df498285db526907737960
tdnguyen <tdnguyen.uet@gmail.com>
2020-07-23 14:30:59 +0700

    initialize

---
+++ src/main/java/com/example/ResearchGate/ResearchGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResearchGate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchGateApplication.class, args);
+	}
+
+}
