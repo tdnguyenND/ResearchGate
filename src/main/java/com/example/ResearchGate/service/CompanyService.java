656bb589a9bc2757fe8970c6f332e9ac1fdd9bbd
tdnguyen <tdnguyen.uet@gmail.com>
2020-08-08 08:23:14 +0700

    company update info controller

--- src/main/java/com/example/ResearchGate/service/CompanyService.java
+++ src/main/java/com/example/ResearchGate/service/CompanyService.java
@@ -3,7 +3,11 @@ package com.example.ResearchGate.service;
 import com.example.ResearchGate.model.Company;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface CompanyService {
+    Optional<Company> findById(Integer userId);
     <S extends Company> S save(S s);
+    void updateInfo(Company company);
 }
--- src/main/java/com/example/ResearchGate/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/example/ResearchGate/service/impl/CompanyServiceImpl.java
@@ -6,13 +6,27 @@ import com.example.ResearchGate.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
     CompanyRepository repository;
+
+    @Override
+    public Optional<Company> findById(Integer userId) {
+        return repository.findById(userId);
+    }
+
     @Override
     public <S extends Company> S save(S s) {
         return repository.save(s);
+    }
+
+    @Override
+    public void updateInfo(Company company) {
+        repository.deleteById(company.userId);
+        repository.save(company);
     }
 }
