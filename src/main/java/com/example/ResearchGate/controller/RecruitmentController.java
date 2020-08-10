6c52227c9c5454003bd85925a0766a4eabdeee0f
darealgautruc <iamvhhoang@gmail.com>
2020-08-11 00:16:07 +0700

    recruitment controller

---
+++ src/main/java/com/example/ResearchGate/controller/RecruitmentController.java
@@ -0,0 +1,36 @@
+package com.example.ResearchGate.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.ResearchGate.model.Recruitment;
+import com.example.ResearchGate.service.RecruitmentService;
+
+@Controller()
+public class RecruitmentController {
+	
+	@Autowired
+	RecruitmentService recruitmentService;
+	
+	@PostMapping("/recruitment")
+	public void recruitmentPosting(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Recruitment recruitment = new Recruitment();
+		recruitment.career = request.getParameter("career");
+		String amount = request.getParameter("amount");
+		recruitment.amount = Integer.parseInt(amount);
+		recruitment.salary = request.getParameter("salary");
+		recruitment.requirement = request.getParameter("requirement");
+		recruitment.advantage = request.getParameter("advantage");
+		recruitment.typeOfWork = request.getParameter("type_of_work");
+		
+		recruitmentService.save(recruitment);
+		response.sendRedirect("../recruitment");
+	}
+	
+}
