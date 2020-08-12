564e91a057ab732b4447ee5223a9a1668c75193c
quangphong12 <56974275+quangphong12@users.noreply.github.com>
2020-08-12 22:38:43 +0700

    Create ApplicationRespository.java

---
+++ src/main/java/com/example/ResearchGate/Repository/ApplicationRespository.java
@@ -0,0 +1,7 @@
+package com.example.ResearchGate.Repository;
+
+import com.example.ResearchGate.model.Application;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<Application, Integer> {
+}
